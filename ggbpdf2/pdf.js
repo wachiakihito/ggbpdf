a3a4d21fc59fe06d3dbd222cc61d69c24f54fee8
和地 輝仁 <wachiakihito@dorff.local>
2023-12-11 20:29:16 +0900

    support line style, caption

--- ggbpdf2/pdf.js
+++ ggbpdf2/pdf.js
@@ -1,51 +1,99 @@
 // -*- coding: utf-8; mode: javascript; mode: outline-minor; js-indent-level: 2 -*-
 //// pdf描画
 class PDF {
+  // 線分のテンプレート
+  static DLINE_DASH = [6, 3];
+  //// 座標をviewBox中心を原点とする右手系に変換
+  // 画像サイズはsvgと同じで決め打ち
+  static conv(x, y) {
+    return [x + SVG.width/2, y + SVG.height/2]
+  }
+
+  //// 線分のスタイル
+  static dashstyle(num) {
+    switch (num) {
+      case 0:  return null;
+      case 15: return [6, 6];
+      case 10: return [4, 4];
+      case 20: return [2, 2];
+      case 30: return [6, 3, 1, 3];
+    }
+    return null;
+  }
+
+  //// 線分を描画キューに追加
+  static line(seg, style) {
+    var scale = SVG.ggb.camera.scale;
+    var [[x1, y1], [x2, y2]] = seg;
+    [x1, y1] = PDF.conv(x1*scale, y1*scale);
+    [x2, y2] = PDF.conv(x2*scale, y2*scale);
+    var dash = PDF.dashstyle(style.lineType);
+    var option = {start:{x:x1, y:y1}, 
+		  end:{x:x2, y:y2},
+		  thickness:style.thickness/3,
+		 };
+    if (dash) { option.dashArray = dash };
+    PDF.page.drawLine(option);
+  }
+
+  //// 破線を描画キューに追加
+  static dline(seg, style) {
+    var scale = SVG.ggb.camera.scale;
+    var [[x1, y1], [x2, y2]] = seg;
+    [x1, y1] = PDF.conv(x1*scale, y1*scale);
+    [x2, y2] = PDF.conv(x2*scale, y2*scale);
+    var option = {start:{x:x1, y:y1}, 
+		  end:{x:x2, y:y2},
+		  thickness:style.thickness/3,
+		  dashArray:PDF.DLINE_DASH
+		 };
+
+    PDF.page.drawLine(option);
+  }
+
+  //// テキストを描画
+  static text(xy2d, uv, txt) {
+    var scale = SVG.ggb.camera.scale;
+    var [x, y] = PDF.conv(xy2d[0]*scale, xy2d[1]*scale);
+    x += uv[0];
+    y -= uv[1]; // PDFはy軸上向き、オフセット下向き
+    // 誤差吸収ハードコード
+    x += 4
+    y += 2
+    PDF.page.drawText(txt, {x:x, y:y, size:14});
+  }
+
   //// pdf描画
+  // SVGの情報を流用する
   static async render() {
     if (! GGBPDF.pdf) { return };
+    var hidden = document.getElementById('hidden-line').checked; // 陰線表示するか
     // pdf作成
-    const pdfDoc = await PDFLib.PDFDocument.create();
-    if (! pdfDoc) { return };
-    const page = pdfDoc.addPage([GGBPDF.PDFW, GGBPDF.PDFW]);
-    // 描画すべき線分
-    var vsegs2d = SVG.hlr.vsegs2d;
-    var isegs2d = SVG.hlr.isegs2d;
-    var hidden = document.getElementById('hidden-line').checked; // 陰線表示
-    // 線分
-    [vsegs2d, isegs2d].forEach ((segs, i) => {
-      if (i == 0 || hidden) {
-        segs.forEach ((seg) => {
-          var [[x1,y1],[x2,y2]] = seg;
-          x1 = x1 * GGBPDF.ggb.camera.scale + GGBPDF.PDFW/2;
-          y1 = y1 * GGBPDF.ggb.camera.scale + GGBPDF.PDFW/2;
-          x2 = x2 * GGBPDF.ggb.camera.scale + GGBPDF.PDFW/2;
-          y2 = y2 * GGBPDF.ggb.camera.scale + GGBPDF.PDFW/2;
-          if (i == 0) {
-            page.drawLine({start:{x:x1,y:y1},end:{x:x2,y:y2}});
-          } else {
-            page.drawLine({start:{x:x1,y:y1},end:{x:x2,y:y2},dashArray:[2,2]});
-          };
-        });
-      }
-    });
+    PDF.pdfDoc = await PDFLib.PDFDocument.create();
+    if (! PDF.pdfDoc) { return };
+    PDF.page = PDF.pdfDoc.addPage([GGBPDF.PDFW, GGBPDF.PDFW]);
+    // 見える線を描画
+    SVG.hlr.vsegs2d.forEach ((seg) => PDF.line(seg, SVG.hlr.vstyles[seg]));
+    // 見えない線を描画
+    if (hidden === true) {
+      SVG.hlr.isegs2d.forEach ((seg) => PDF.dline(seg, SVG.hlr.istyles[seg]));
+    }
     // 表示ラベルの描画
-    var ggb = GGBPDF.ggb;
+    var ggb = SVG.ggb;
     for (let lbl of Object.keys(ggb.pts)) {
       if (! ggb.elts[lbl].showobj || ! ggb.elts[lbl].showlbl) { continue };
       let xyz = V.transform(ggb.pts[lbl],
                             ggb.camera.xAngle, ggb.camera.zAngle,
                             ggb.camera.xZero, ggb.camera.yZero, ggb.camera.zZero);
-      let [x, y] = V.proj(ggb.camera.eyex, ggb.camera.scrnx, xyz)[0];
-      let [u, v] = ggb.elts[lbl].labelOffset;
-      x = x * GGBPDF.ggb.camera.scale + GGBPDF.PDFW/2 + u;
-      y = y * GGBPDF.ggb.camera.scale + GGBPDF.PDFW/2 - v;
-      x += 5; // 誤差吸収ハードコード
-      y += 7; // 誤差吸収ハードコード
-      page.drawText(lbl, {x:x, y:y, size:14});
+      let xy2d = V.proj(ggb.camera.eyex, ggb.camera.scrnx, xyz)[0];
+      if (ggb.elts[lbl].labelText[0] == '<') { // texは<span>で始まるはず。点の名前を表示
+	PDF.text(xy2d, ggb.elts[lbl].labelOffset, lbl);
+      } else { // 文字列
+	PDF.text(xy2d, ggb.elts[lbl].labelOffset, ggb.elts[lbl].labelText);
+      }
     }
-    //
-    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    // 結果を表示
+    const pdfDataUri = await PDF.pdfDoc.saveAsBase64({ dataUri: true });
     GGBPDF.pdf.src = pdfDataUri;
   }
 } // end of class PDF
